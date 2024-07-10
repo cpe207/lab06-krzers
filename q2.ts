17e235e87f846d459e5a2cda7184b2162a3fd293
krzers <tyhffggfg@gmail.com>
2024-07-11 00:52:32 +0700

    finish

--- q2.ts
+++ q2.ts
@@ -3,7 +3,28 @@ import axios from "axios";
 
 /* assign interface/type to the function definition properly */
 const getTodo = async (todoId: number) => {
-  /* Your code here */
+  try{
+    const todo_res = await axios.get(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+
+    try{
+      const id = todo_res.data.userId;
+      const person_res = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+      const return_obj = {
+        "owner" : `${person_res.data.name}`,
+        "title" : `${todo_res.data.title}` ,
+        "completed": todo_res.data.completed,
+      };
+      return return_obj;
+    }
+
+    catch(err){
+      return "INVALID TODO ID";
+    }
+  }
+  
+  catch(err){
+    return "INVALID TODO ID";
+  }
 };
 
 //test case
